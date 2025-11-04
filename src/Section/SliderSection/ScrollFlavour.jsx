611a11485468434db0404127ed6c848a61c7d5da
DoctorJunaid <junaid.aurangzeb1@gmail.com>
2025-11-04 17:35:49 +0500

    workign on horizontal scrolling section

--- src/Section/SliderSection/ScrollFlavour.jsx
+++ src/Section/SliderSection/ScrollFlavour.jsx
@@ -10,9 +10,9 @@
                     flavorlists.map((item, index) => {
                         return (
                         <div key={index} className={`relative z-30 lg:w-[50vw] w-80 lg:h-[70vh] md:w-[90vw] md:h-[50vh] h-80 flex-none ${item.rotation}`}>
-                               <img src={`/src/assets/images/${item.color}-bg.svg`} alt="" className={"absolute bottom-0 left-0 right-0  lg:w-full "} />
-                                <img src={`/src/assets/images/${item.color}-drink.webp`} alt=""  className={"absolute z-50 left-1/2 md:left-[33%] -translate-x-1/2 bottom-0 md:h-full h-80 w-fit "} />
-                                <img src={`/src/assets/images/${item.color}-elements.webp`} alt=""  className={"absolute left-0 z-40 md:top-0 md:bottom-auto bottom-10 w-full md:h-full  "}/>
+                               <img src={`/images/${item.color}-bg.svg`} alt="" className={"absolute bottom-0 left-0 right-0  lg:w-full "} />
+                                <img src={`/images/${item.color}-drink.webp`} alt=""  className={"absolute z-50 left-1/2 md:left-[33%] -translate-x-1/2 bottom-0 md:h-full h-80 w-fit "} />
+                                <img src={`/images/${item.color}-elements.webp`} alt=""  className={"absolute left-0 z-40 md:top-0 md:bottom-auto bottom-10 w-full md:h-full  "}/>
                                 <h1 className={" absolute z-60  bottom-6 left-6 text-xl md:text-4xl lg:text-8xl font-bold text-[#FAEADE] "}>{item.name}</h1>
                             </div>
                         )
